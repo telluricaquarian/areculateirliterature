0ada99cf55fcead342def3d1a476da18a25f2de5
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2026-04-04 21:18:19 -0700

    feat: set Areculateir Aa rounded icon as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,10 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Areculateir',
+  description: 'Areculateir',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/roundedfavicon.png',
   },
 }
 
